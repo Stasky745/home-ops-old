9014cda3fc2001697da3d6bbb7ad6d7149099d7f
Stasky745 <github.dev@stasky.anonaddy.me>
2023-02-01 23:29:39 +0100

    Initial commit single master node :rocket:

--- terraform/proxmox/masters.tf
+++ terraform/proxmox/masters.tf
@@ -11,16 +11,16 @@ module "master-1" {
   node-ipconfig = "ip=192.168.1.201/24,gw=192.168.1.1"
 }
 
-module "master-2" {
-  source        = "./modules/master-node"
-  node-id       = 202
-  node-name     = "k8s-m2"
-  node-ipconfig = "ip=192.168.1.202/24,gw=192.168.1.1"
-}
-
-module "master-3" {
-  source        = "./modules/master-node"
-  node-id       = 203
-  node-name     = "k8s-m3"
-  node-ipconfig = "ip=192.168.1.203/24,gw=192.168.1.1"
-}
+#module "master-2" {
+#  source        = "./modules/master-node"
+#  node-id       = 202
+#  node-name     = "k8s-m2"
+#  node-ipconfig = "ip=192.168.1.202/24,gw=192.168.1.1"
+#}
+#
+#module "master-3" {
+#  source        = "./modules/master-node"
+#  node-id       = 203
+#  node-name     = "k8s-m3"
+#  node-ipconfig = "ip=192.168.1.203/24,gw=192.168.1.1"
+#}
